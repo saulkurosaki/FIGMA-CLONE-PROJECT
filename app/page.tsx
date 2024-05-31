dc86bf78aa7edd658fe6cff7dc1be5246635c78e
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-31 15:23:04 -0600

    feat(layout.tsx): implement the complete layout.tsx component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import { Room } from "./Room";
-import { CollaborativeApp } from "./CollaborativeApp";
 
 export default function Page() {
-  return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
-  );
+  return <h1 className="font-xl text-white">LiveBlocks Figma Clone</h1>;
 }
