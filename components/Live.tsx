e22fc695be6cc70a1777dc3d6f71ea58ef0449a3
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-31 19:13:04 -0600

    feat(Live): implement the handlePointerMove function

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,9 +1,21 @@
-import { useOthers } from "@/liveblocks.config";
+import { useMyPresence, useOthers } from "@/liveblocks.config";
 
 import LiveCursors from "./cursor/LiveCursors";
+import React, { useCallback } from "react";
 
 const Live = () => {
   const others = useOthers();
+
+  const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+
+  const handlePointerMove = useCallback((event: React.PointerEvent) => {
+    event.preventDefault();
+
+    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+
+    updateMyPresence({ cursor: { x, y } });
+  }, []);
 
   return (
     <div>
