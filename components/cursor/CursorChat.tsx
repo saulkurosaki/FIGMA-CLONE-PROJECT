58c88bd1276c08920e1ca3e8859527540c917436
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-31 19:43:26 -0600

    feat(CursorChat): implement the cursor message input field box

--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -1,4 +1,5 @@
-import { CursorChatProps } from "@/types/type";
+import CursorSVG from "@/public/assets/CursorSVG";
+import { CursorChatProps, CursorMode } from "@/types/type";
 
 const CursorChat = ({
   cursor,
@@ -12,7 +13,23 @@ const CursorChat = ({
       style={{
         transform: `translateX(${cursor.x}px) translateY(${cursor.y}px)`,
       }}
-    ></div>
+    >
+      {/* {cursorState.mode === CursorMode.Chat && ( */}
+      <>
+        <CursorSVG color="#000" />
+
+        <div className="absolute left-2 top-5 bg-blue-500 px-4 py-2 text-sm leading-relaxed text-white rounded-[20px]">
+          {cursorState.previousMessage && (
+            <div>{cursorState.previousMessage}</div>
+          )}
+          <input
+            className="z-10 w-60 border-none bg-transparent text-white placeholder-blue-300 outline-none"
+            autoFocus={true}
+          />
+        </div>
+      </>
+      {/* )} */}
+    </div>
   );
 };
 
