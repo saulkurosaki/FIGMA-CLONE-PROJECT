74d9b44d5cff22a4c1e28dbed89ae31566f8812c
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-31 19:07:53 -0600

    feat(page.tsx): implement the Live component within the Page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
+import Live from "@/components/Live";
 import { Room } from "./Room";
 
 export default function Page() {
   return (
     <div className="w-full h-[100vh] flex justify-center items-center text-center">
       <h1 className="text-2xl text-white">LiveBlocks Figma Clone</h1>
+
+      <Live />
     </div>
   );
 }
