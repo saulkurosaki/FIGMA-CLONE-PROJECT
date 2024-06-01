ca92e7f231f0027d3d88d262ee417c1a6c738aff
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-31 19:28:01 -0600

    feat(CursorChat): implement the component container layout along as the component accepted props

--- components/cursor/CursorChat.tsx
+++ components/cursor/CursorChat.tsx
@@ -1,5 +1,19 @@
-const CursorChat = () => {
-  return <div>CursorChat</div>;
+import { CursorChatProps } from "@/types/type";
+
+const CursorChat = ({
+  cursor,
+  cursorState,
+  setCursorState,
+  updateMyPresence,
+}: CursorChatProps) => {
+  return (
+    <div
+      className="absolute top-0 left-0"
+      style={{
+        transform: `translateX(${cursor.x}px) translateY(${cursor.y}px)`,
+      }}
+    ></div>
+  );
 };
 
 export default CursorChat;
