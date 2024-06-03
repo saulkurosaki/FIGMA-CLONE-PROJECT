b95d5f8c865e9aa16404d50538dfb08270ae0c94
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-03 09:31:40 -0600

    feat(ActiveUsers): implement the memoized version of the components return

--- components/users/ActiveUsers.tsx
+++ components/users/ActiveUsers.tsx
@@ -3,33 +3,44 @@ import { Avatar } from "./Avatar";
 
 import styles from "./index.module.css";
 import { generateRandomName } from "@/lib/utils";
+import { useMemo } from "react";
 
 const ActiveUsers = () => {
   const users = useOthers();
   const currentUser = useSelf();
   const hasMoreUsers = users.length > 3;
 
-  return (
-    <div className="flex items-center justify-center gap-1">
-      <div className="flex pl-3">
-        {currentUser && (
-          <Avatar name="You" otherStyles="border-[3px] border-primary-green" />
-        )}
-
-        {users.slice(0, 3).map(({ connectionId }) => {
-          return (
+  const memoizedUsers = useMemo(
+    () => (
+      <div className="flex items-center justify-center gap-1">
+        <div className="flex pl-3">
+          {currentUser && (
             <Avatar
-              key={connectionId}
-              name={generateRandomName()}
-              otherStyles="-ml-3"
+              name="You"
+              otherStyles="border-[3px] border-primary-green"
             />
-          );
-        })}
+          )}
 
-        {hasMoreUsers && <div className={styles.more}>+{users.length - 3}</div>}
+          {users.slice(0, 3).map(({ connectionId }) => {
+            return (
+              <Avatar
+                key={connectionId}
+                name={generateRandomName()}
+                otherStyles="-ml-3"
+              />
+            );
+          })}
+
+          {hasMoreUsers && (
+            <div className={styles.more}>+{users.length - 3}</div>
+          )}
+        </div>
       </div>
-    </div>
+    ),
+    [users.length]
   );
+
+  return memoizedUsers;
 };
 
 export default ActiveUsers;
