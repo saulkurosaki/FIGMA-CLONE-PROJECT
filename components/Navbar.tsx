63109dc8bc74d6f8a472332dc205fcf4830478da
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-03 09:22:58 -0600

    feat(Navbar): implement the ActriveUsers component within the Navbar one

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,6 +11,8 @@ const Navbar = ({ activeElement }: NavbarProps) => {
   return (
     <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
       <Image src="/assets/logo.svg" alt="FigPro Logo" width={58} height={20} />
+
+      <ActiveUsers />
     </nav>
   );
 };
