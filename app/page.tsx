ab50646be7e01c1cf2071397e0bb66a665ca53bb
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-03 14:42:20 -0600

    feat(page.tsx): implement the deleteAllShapes function along as the reset button feature

--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,7 @@ import {
 } from "@/lib/canvas";
 import { ActiveElement } from "@/types/type";
 import { useMutation, useStorage } from "@/liveblocks.config";
+import { defaultNavElement } from "@/constants";
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -49,8 +50,33 @@ export default function Page() {
     canvasObjects.set(objectId, shapeData);
   }, []);
 
+  const deleteAllShapes = useMutation(({ storage }) => {
+    const canvasObjects = storage.get("canvasObjects");
+
+    if (!canvasObjects || canvasObjects.size === 0) {
+      return true;
+    }
+
+    for (const [key, value] of canvasObjects.entries()) {
+      canvasObjects.delete(key);
+    }
+
+    return canvasObjects.size === 0;
+  }, []);
+
   const handleActiveElement = (elem: ActiveElement) => {
     setActiveElement(elem);
+
+    switch (elem?.value) {
+      case "reset":
+        deleteAllShapes();
+        fabricRef.current?.clear();
+        setActiveElement(defaultNavElement);
+        break;
+
+      default:
+        break;
+    }
 
     selectedShapeRef.current = elem?.value as string;
   };
