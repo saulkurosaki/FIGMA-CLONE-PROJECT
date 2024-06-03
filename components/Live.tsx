750d51168edc49bfcf2a940bf4cd513973a83065
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-03 12:55:41 -0600

    feat(page.tsx): implement the canvas rectangle shape append when mouse press feature

--- components/Live.tsx
+++ components/Live.tsx
@@ -13,7 +13,11 @@ import ReactionSelector from "./reaction/ReactionButton";
 import FlyingReaction from "./reaction/FlyingReaction";
 import useInterval from "@/hooks/useInterval";
 
-const Live = () => {
+type Props = {
+  canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
+};
+
+const Live = ({ canvasRef }: Props) => {
   const others = useOthers();
 
   const [{ cursor }, updateMyPresence] = useMyPresence() as any;
@@ -155,13 +159,14 @@ const Live = () => {
 
   return (
     <div
+      id="canvas"
       onPointerMove={handlePointerMove}
       onPointerLeave={handlePointerLeave}
       onPointerDown={handlePointerDown}
       onPointerUp={handlePointerUp}
       className="w-full h-[100vh] flex justify-center items-center text-center"
     >
-      <h1 className="text-2xl text-white">LiveBlocks Figma Clone</h1>
+      <canvas ref={canvasRef} />
 
       {reactions.map((r) => (
         <FlyingReaction
