54d36358dc96500d987b61391a6ca7bfbb6e0ff3
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-02 23:36:54 -0600

    feat(page.tsx): implement the Navbar component within the main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@
 
 import Live from "@/components/Live";
 import { Room } from "./Room";
+import Navbar from "@/components/Navbar";
 
 export default function Page() {
   return (
     <div>
+      <Navbar />
       <Live />
     </div>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,21 @@
-const Navbar = () => {
-  return <div>Navbar</div>;
+"use client";
+
+import Image from "next/image";
+import { memo } from "react";
+
+import { NavbarProps } from "@/types/type";
+
+import ActiveUsers from "./users/ActiveUsers";
+
+const Navbar = ({ activeElement }: NavbarProps) => {
+  return (
+    <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
+      <Image src="/assets/logo.svg" alt="FigPro Logo" width={58} height={20} />
+    </nav>
+  );
 };
 
-export default Navbar;
+export default memo(
+  Navbar,
+  (prevProps, nextProps) => prevProps.activeElement === nextProps.activeElement
+);
