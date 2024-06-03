3ca3b83515f30fc783ff989f126ca05dfd7a568f
saulkurosaki <saulkurosaki13@gmail.com>
2024-06-03 13:01:17 -0600

    feat(Navbar): implement the nav elements iterations and ternaries displays

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,14 +3,29 @@
 import Image from "next/image";
 import { memo } from "react";
 
-import { NavbarProps } from "@/types/type";
+import { ActiveElement, NavbarProps } from "@/types/type";
 
 import ActiveUsers from "./users/ActiveUsers";
+import { navElements } from "@/constants";
 
 const Navbar = ({ activeElement }: NavbarProps) => {
   return (
     <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white">
       <Image src="/assets/logo.svg" alt="FigPro Logo" width={58} height={20} />
+
+      <ul className="flex flex-row">
+        {navElements.map((item: ActiveElement | any) => (
+          <li key={item.name}>
+            {Array.isArray(item.value) ? (
+              <ShapesMenu />
+            ) : item?.value === "comments" ? (
+              <NewThread></NewThread>
+            ) : (
+              <button></button>
+            )}
+          </li>
+        ))}
+      </ul>
 
       <ActiveUsers />
     </nav>
